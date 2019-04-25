ac0ec861b4d199164fa79c22c849da948eead024
ahidjon <ahidjon97@gmail.com>
2019-04-26 05:52:55 +0900

    Modifyed function

--- gugudan.py
+++ gugudan.py
@@ -1,4 +1,8 @@
-for i in range(1,10):
-    for j in range(1,10):
-        print(i," x ",j, " = ", i*j)
-    print()
+def gugudan(n):
+    for i in range(1,n):
+        for j in range(1,n):
+            print(i," x ",j, " = ", i*j,  end="\t")
+        print()
+
+gugudan(10)
+
